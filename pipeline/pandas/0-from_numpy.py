99b4558fd50d950f71d1b0d64299257279420063
root <root@f68c3b16108148ea80e4f36e72a7780d-2377118072>
2026-01-09 09:46:48 +0000

    p

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-import pandas as pd
 """
-Module 0-from_numpy
+0-from_numpy module
+
 Creates a pandas DataFrame from a NumPy ndarray.
 """
 
+import pandas as pd
+
 
 def from_numpy(array):
-
     """
+    Creates a pandas DataFrame from a NumPy ndarray.
+
+    The columns are labeled in alphabetical order using
+    capital letters (A–Z).
+
     Args:
-        array (numpy.ndarray): The input NumPy array.
+        array (numpy.ndarray): Input NumPy array
 
     Returns:
-        pandas.DataFrame: DataFrame with labeled columns.
-
-    Columns are labeled with capital letters in alphabetical order.
+        pandas.DataFrame: DataFrame with labeled columns
     """
     n_cols = array.shape[1]
     columns = [chr(65 + i) for i in range(n_cols)]
