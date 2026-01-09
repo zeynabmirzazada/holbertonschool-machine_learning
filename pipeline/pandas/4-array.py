ac53792f8e6bdbd9b1679f8d397fcc9535deda30
root <root@f68c3b16108148ea80e4f36e72a7780d-2377118072>
2026-01-09 11:23:00 +0000

    p

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -10,4 +10,7 @@ def array(df):
     select only High and Close columns
     convert last 10 rows into ndarray
     '''
-    return df[['High', 'Close']].tail(10).to_numpy()
+    df = df[['High', 'Close']]
+    df = df.tail(10)
+    df = df.to_numpy()
+    return df
