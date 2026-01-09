4cd150353c9261afa3c9d4a519670304728e8bd4
root <root@f68c3b16108148ea80e4f36e72a7780d-2377118072>
2026-01-09 12:31:24 +0000

    p

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -4,7 +4,6 @@ function that takes a pd.DataFrame and extracts the columns High, Low, Close,
 and Volume_BTC. Then selects every 60th row from these columns.
 Returns: the sliced pd.DataFrame
 '''
-import pandas as pd
 
 
 def slice(df):
@@ -12,5 +11,5 @@ def slice(df):
     df - dataframe
     return: sliced df with every 60th row
     '''
-    df = df[['High', 'Low', 'Close', 'Volume_BTC']]
+    df = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
     return df.iloc[1500::60]
