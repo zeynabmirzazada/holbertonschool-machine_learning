56a9e842381c05d5daab79346919b6baee9911c6
root <root@f68c3b16108148ea80e4f36e72a7780d-2377118072>
2026-01-09 10:02:08 +0000

    p

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -7,4 +7,4 @@ df = pd.DataFrame(
         {
             'First': [0.0, 0.5, 1.0, 1.5],
             'Second': ['one', 'two', 'three', 'four'],
-        }, index = list('ABCD'))
+        }, index=list('ABCD'))
