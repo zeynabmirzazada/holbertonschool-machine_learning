db7057cd5600accd9149f4c0a19fde793cb1e1ad
root <root@f68c3b16108148ea80e4f36e72a7780d-2377118072>
2026-01-09 11:18:52 +0000

    p

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+function that takes dataframe and converts last 10 rows into numpy ndarray
+'''
+import pandas as pd
+
+
+def array(df):
+    '''
+    select only High and Close columns
+    convert last 10 rows into ndarray
+    '''
+    return df[['High', 'Close']].to_numpy()
