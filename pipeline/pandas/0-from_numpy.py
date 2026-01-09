bfacd89b29422985e9f84b8de0ba0875595dc13d
root <root@f68c3b16108148ea80e4f36e72a7780d-2377118072>
2026-01-09 09:39:36 +0000

    p

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -11,8 +11,6 @@ def from_numpy(array):
     Columns are labeled with capital letters in alphabetical order.
     """
     n_cols = array.shape[1]
-
-    # Generate column labels using ASCII codes
     columns = [chr(65 + i) for i in range(n_cols)]
 
     return pd.DataFrame(array, columns=columns)
