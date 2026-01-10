aebd830abef7bd4db2927670cb46c04de5f473f5
root <root@137fa147e38b4b3ab5cde42519ed15ca-2377118072>
2026-01-10 09:27:27 +0000

    hj

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -25,4 +25,5 @@ def hierarchy(df1, df2):
     df2 = df2.loc[1417411980:1417417980]
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
     df = df.swaplevel(0, 1)
-    return df.sort_index()
+    df = df.sort_index()
+    return df
