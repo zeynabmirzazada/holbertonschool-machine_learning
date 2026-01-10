245404de7571ed8b471cc1934eefa1d11f2c17aa
root <root@137fa147e38b4b3ab5cde42519ed15ca-2377118072>
2026-01-10 08:57:05 +0000

    hj

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -21,7 +21,6 @@ def hierarchy(df1, df2):
     '''
     df1 = index(df1)
     df2 = index(df2)
-    df = pd.concat([df2.loc[1417411980:1417417980],
-        df1.loc[1417411980:1417417980]], keys=['bitstamp', 'coinbase'])
-    df.sort_index()
-    return df
+    return pd.concat([df2.loc[1417411980:1417417980],
+        df1.loc[1417411980:1417417980]],
+        keys=['bitstamp', 'coinbase']).sort_index((
