110f8beeae99ec7a7ce3358d5f0abd87e5abdb90
root <root@137fa147e38b4b3ab5cde42519ed15ca-2377118072>
2026-01-10 09:33:58 +0000

    hj

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 '''
-function that takes two pd.DataFrame objects and rearranges the MultiIndex so
-that Timestamp is the first level. Concatenates the bitstamp and coinbase
-tables from timestamps 1417411980 to 1417417980, inclusive. Adds keys to the
-data, labeling rows from df2 as bitstamp and rows from df1 as coinbase.
-Ensures the data is displayed in chronological order.
-You should use index = __import__('10-index').index.
-Returns: the concatenated pd.DataFrame.
+This module creates a function
+to concatenate 2 dataframes
 '''
 import pandas as pd
 index = __import__('10-index').index
@@ -14,14 +9,12 @@ index = __import__('10-index').index
 
 def hierarchy(df1, df2):
     '''
-    function that takes two pd.DataFrame objects and rearranges the MultiIndex
-    that Timestamp is the first level. Concatenates the bitstamp and coinbase
-    tables from timestamps 1417411980 to 1417417980, inclusive. Adds keys to
-    data, labeling rows from df2 as bitstamp and rows from df1 as coinbase.
+    this function concatenates
+    two dataframes
     '''
     df1 = index(df1)
     df2 = index(df2)
-    df1 = df1.loc[1417411980:1417417980]
-    df2 = df2.loc[1417411980:1417417980]
+    df1 = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
+    df2 = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
     return pd.concat([df2, df1], keys=['bitstamp', 'coinbase']).\
-            swaplevel(0, 1).sort_index()
+        reorder_levels([1, 0]).sort_index()
