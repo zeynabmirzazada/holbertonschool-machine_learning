fe0eb11f5fc8ce8bbd09d04113e8793c2c67f91f
root <root@157c1d1f167d46fc9a27eca7265d90e6-2377118072>
2026-01-10 15:00:45 +0000

    pl

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that plots a scatter plot of
+height versus weight data.
+"""
+
+
+def scatter():
+    """
+    Plots a scatter plot of Men's Height vs Weight.
+    The points are magenta, axes are labeled, and a title is included.
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot the scatter points in magenta
+    plt.scatter(x, y, c='m')
+
+    # Add labels and title
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+
+    # Display the plot
+    plt.show()
