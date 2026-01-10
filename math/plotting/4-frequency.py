d6d3ed12b1565c8efbde077be1bc6a543c3c275d
root <root@e15e2bb700434a96a361e2241e99c5fe-2377118072>
2026-01-10 17:10:07 +0000

    khkj

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -17,6 +17,8 @@ def frequency():
 
     # Define bins every 10 units from 0 to 100
     bins = list(range(0, 101, 10))
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
 
     # Plot the histogram
     # edgecolor='black' adds the requested outline
@@ -26,6 +28,7 @@ def frequency():
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.xticks(bins)
 
     # Display the plot
     plt.show()
