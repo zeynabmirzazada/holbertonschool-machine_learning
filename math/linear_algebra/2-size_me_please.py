574bf04ec589665b6cd530ade020bff38d8e2bec
root <root@0f18f8d7eddb4e049b43294a25bce6c7-2377118072>
2026-01-20 17:47:23 +0000

    jvj

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -7,4 +7,4 @@ def matrix_shape(matrix):
     assume all elements in the same dimension are of the same type/shape
     The shape should be returned as a list of integers
     '''
-    return list(np.array(matrix).shape)
+    return list(numpy.array(matrix).shape)
