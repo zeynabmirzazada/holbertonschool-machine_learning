ad1a3a18428d92371f1b0b571920505ad42cff78
root <root@0f18f8d7eddb4e049b43294a25bce6c7-2377118072>
2026-01-20 18:47:51 +0000

    jvj

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -2,15 +2,17 @@
 '''
 function that returns the transpose of a 2D matrix, matrix
 '''
+
+
 def matrix_transpose(matrix):
     '''
     You must return a new matrix
     You can assume that matrix is never empty
     You can assume all elements in the same dimension are of same type/shape
     '''
-    new=[]
+    new = []
     for i in range(len(matrix[0])):
-        sub_new=[]
+        sub_new = []
         for k in range(len(matrix)):
             sub_new.append(matrix[k][i])
         new.append(sub_new)
