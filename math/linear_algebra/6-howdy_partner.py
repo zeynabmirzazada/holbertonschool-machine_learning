e49507204397448231ba853b816c382da72f7d8f
root <root@0f18f8d7eddb4e049b43294a25bce6c7-2377118072>
2026-01-20 22:26:32 +0000

    jvj

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+this module provides function that concatenates 2 arrays
+'''
+
+
+def cat_arrays(arr1, arr2):
+    '''
+    args:
+        arr1 - list of int/floats
+        arr2 - list of int/floats
+    return new list
+    '''
+    return arr1 + arr2
