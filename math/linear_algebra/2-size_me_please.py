e6f34d99ebfd66dfed542d38166a977cfe0caa2b
root <root@0f18f8d7eddb4e049b43294a25bce6c7-2377118072>
2026-01-20 17:45:41 +0000

    jvj

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+function that calculates the shape of a matrix
+'''
+def matrix_shape(matrix):
+    '''
+    assume all elements in the same dimension are of the same type/shape
+    The shape should be returned as a list of integers
+    '''
+    return list(np.array(matrix).shape)
