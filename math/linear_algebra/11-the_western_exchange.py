dccd4f511218fb5e3663a0b1dffb3164418a44bd
root <root@4831a8d86cdc41b8ae8985b89f7cf6b8-2377118072>
2026-01-24 11:57:51 +0000

    fhfgh

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to transpose a matrix.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a matrix.
+
+    Args:
+        matrix (numpy.ndarray): Input matrix
+
+    Returns:
+        numpy.ndarray: Transposed matrix
+    """
+    return matrix.transpose()
