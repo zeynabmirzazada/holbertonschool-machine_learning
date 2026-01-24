013bedc0a9379fdd4c8ad0e146b609ab85f5bebb
root <root@4831a8d86cdc41b8ae8985b89f7cf6b8-2377118072>
2026-01-24 11:54:02 +0000

    fhfgh

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to calculate the shape of a numpy.ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): Input array
+
+    Returns:
+        tuple: Shape of the array
+    """
+    return matrix.shape
