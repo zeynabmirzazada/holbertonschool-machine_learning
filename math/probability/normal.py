116d05b199a9a848c39243e7e11bcc2d297398a5
root <root@29b3f1d73eeb46ffa58c4380b9206936-2377118072>
2026-02-06 19:05:29 +0000

    gfg

---
+++ math/probability/normal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''module documented'''
+
+
+class Normal:
+    '''class documented'''
+    def __init__(self, data=None, mean=0., stddev=1.):
+        '''constructor documented'''
+        self.data = data
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = mean
+            self.stddev = stddev
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = self.standard_dev()
+
+    def standard_dev(self):
+        '''method documented'''
+        stddev = 0
+        for i in range(len(self.data)):
+            stddev = stddev + (self.data[i] - self.mean) ** 2
+        stddev = (stddev / len(self.data)) ** (0.5)
+        return stddev
