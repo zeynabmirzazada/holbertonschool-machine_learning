ca32a758a15a29c510dd368243d78cbe142c3742
root <root@4d101d57b7f44b8387a160068ff2ac07-2377118072>
2026-02-07 18:01:42 +0000

    dfb

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Calculates the intersection"""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """Comment Bayesian Probability"""
+    if (type(n) is not int) or (n <= 0):
+        raise ValueError("n must be a positive integer")
+    if (type(x) is not int) or (x < 0):
+        raise ValueError("x must be an integer that is greater than or equal "
+                         "to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if (type(P) is not np.ndarray) or (len(P.shape) != 1):
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if (type(Pr) is not np.ndarray) or (P.shape != Pr.shape):
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    for p, pr in zip(P, Pr):
+        if not (p >= 0 and p <= 1):
+            raise ValueError("All values in P must be in the range [0, "
+                             "1]")
+        if not (pr >= 0 and pr <= 1):
+            raise ValueError("All values in Pr must be in the range [0, "
+                             "1]")
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    num = np.math.factorial(n)
+    den = np.math.factorial(x) * np.math.factorial(n - x)
+
+    like = num / den * (P ** x) * ((1 - P) ** (n - x))
+    inter = like * Pr
+
+    return inter
---
+++ math/bayesian_prob/2-marginal.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import numpy as np
+
+
+def factorial(n):
+    """Calculate The Factorial"""
+    f = 1
+    for i in range(1, n + 1):
+        f *= i
+    return f
+
+
+def marginal(x, n, P, Pr):
+    """Calculate The Marginal"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P"
+        )
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    comb = factorial(n) / (factorial(n - x) * factorial(x))
+
+    likelihoods = comb * (P ** x) * ((1 - P) ** (n - x))
+
+    intersections = Pr * likelihoods
+
+    marg = np.sum(intersections)
+
+    return marg
