3cf6d06deb8185e0aae8408e3d5e2a0929afce05
root <root@8121d68b41a449ec9d42f01960f57ce4-2377118072>
2026-03-10 22:00:05 +0000

    keras

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''build model'''
+import keras
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''nx is the number of input features to the network
+    layers - is a list containing the number of nodes in each layer of the
+    network
+    activations - is a list containing the activation functions used for each
+    layer of the network
+    lambtha - is the L2 regularization parameter
+    keep_prob is the probability that a node will be kept for dropout
+    You are not allowed to use the Input class
+    Returns: the keras model'''
+    model = Sequential([
+        Dense(layers[0], kernel_regularizer=regularizers.L2(lambtha),
+            input_shape=(nx,), activation=activations[0]),
+        Dropout(keep_prob),  # Dropout layer with 30% rate
+        Dense(layers[1], kernel_regularizer=regularizers.L2(lambtha),
+            activation=activations[1]),
+        Dropout(keep_prob),
+        Dense(layers[2], kernel_regularizer=regularizers.L2(lambtha),
+            activation=activations[2])
+        ])
