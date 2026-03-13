0db38c1010057a7c192497c6095dcead9829280e
root <root@ip-10-42-225-251>
2026-03-13 23:53:16 +0000

    dg

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
-"""
-Updates the learning rate using inverse time decay with tensorflow
-"""
+"""Learning Rate Decay Upgraded"""
 import tensorflow as tf
 
 
-def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
-    """
-    a function that updates the learning rate with tensorflow
-    :param alpha: the original learning rate
-    :param decay_rate: the weight used to determine the rate at which alpha
-    will decay
-    :param global_step: the number of passes of gradient descent that have
-    elapsed
-    :param decay_step: the number of passes of gradient descent that should
-    occur before alpha is decayed further
-    :return: the learning rate decay operation
-    """
-    # staircase= True will make discrete output as floor(global_step /
-    # decay_step in the calculation)
-    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
-                                       decay_rate, staircase=True)
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """This function creates a learning rate decay
+     operation in tensorflow using inverse time decay"""
+    optimizer = tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )
+    return optimizer
