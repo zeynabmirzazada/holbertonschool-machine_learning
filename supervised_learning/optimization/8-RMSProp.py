9480c6795b1d654f6d7bbda0f51e23f516fb8c0f
root <root@ip-10-42-225-251>
2026-03-13 23:46:05 +0000

    dg

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""
-Updates a variable using the RMSprop optimization algorithm with tensorflow
-"""
+"""RMSProp Upgraded"""
 import tensorflow as tf
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """
-    a function that optimizes using RMSprop with tensorflow
-    :param loss: the loss of the network
-    :param alpha: the learning rate
-    :param beta2: the RMSprop weight
-    :param epsilon: small number to avoid division by zero
-    :return: the RMSprop optimization operation
-    """
-    rms = tf.train.RMSPropOptimizer(alpha, decay=beta2, epsilon=epsilon)
-    return rms.minimize(loss)
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """This function sets up the RMSProp
+     optimization algorithm in TensorFlow"""
+    optimizer = tf.keras.optimizers.RMSprop(
+        alpha, beta2, epsilon
+    )
+    return optimizer
