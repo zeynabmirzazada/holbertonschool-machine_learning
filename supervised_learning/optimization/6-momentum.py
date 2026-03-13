91bab8df4d148d4ec6238738f7e0970adbd2a1bc
root <root@ip-10-42-225-251>
2026-03-13 23:44:39 +0000

    dg

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-"""
-Updates a variable using the gradient descent with momentum with tensorflow
-"""
+"""Momentum Upgraded"""
 import tensorflow as tf
 
 
-def create_momentum_op(loss, alpha, beta1):
-    """
-    a function that uses momentum optimization algorithm with tensorflow
-    :param loss: the loss of the network
-    :param alpha: the learning rate
-    :param beta1: the momentum weight
-    :return: the momentum optimization
-    """
-    # the minimize method of MomentumOptimizer simply the compute_gradients()
-    # and apply_gradients() calls
-    return tf.train.MomentumOptimizer(alpha, momentum=beta1).minimize(loss)
+def create_momentum_op(alpha, beta1):
+    """This function sets up the gradient descent with
+    momentum optimization algorithm in TensorFlow"""
+    optimizer = tf.keras.optimizers.SGD(alpha, beta1)
+    return optimizer
