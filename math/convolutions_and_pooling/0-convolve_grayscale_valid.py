f7318cc334719c0c7d5f3001a847126980251523
root <root@1817051134354b8c9c1990568e64da58-2377118072>
2026-03-26 22:36:53 +0000

    dgd

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''valid convolution on grayscale images'''
+
+
+def convolve_grayscale_valid(images, kernel):
+    '''
+    images is a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for th
+    convolution
+        kh is the height of the kernel
+        kw is the width of the kernel
+    Returns: a numpy.ndarray containing the convolved images'''
+    m, h, w = images.shape[0], images.shape[1], images.shape[2]
+    kh, kw = kernel.shape[0], kernel.shape[1]
+    conv_imgs = np.zeros((m, h - kh + 1, w - kw + 1))
+    for row in range(h - kh + 1):
+        for column in range(w - kw + 1):
+            part = images[:, row:row + kh, column:column + kw] * kernel
+            conv_imgs[:, row, col] = np.sum(part, axis=(1, 2))
+    return conv_imgs
