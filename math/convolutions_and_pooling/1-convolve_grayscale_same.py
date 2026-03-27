f2c71dead085eac3c9a329d1b33663478169fdcf
root <root@1cf4ba4253b34a8ebfe9fc0ba9655fcf-2377118072>
2026-03-27 11:07:20 +0000

    dg

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -17,11 +17,11 @@ def convolve_grayscale_same(images, kernel):
     Returns: a numpy.ndarray containing the convolved images'''
     m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw = kernel.shape[0], kernel.shape[1]
-    conv_imgs = np.zeros((m, h + 2, w + 2))
+    images = np.pad(images, ((0, 0), (1, 1), (1, 1)), 'constant',
+            constant_values=0)
     conv = np.zeros((m, h, w))
-    conv_imgs[:, 1:h + 1, 1:w + 1] = images[:, :, :]
     for row in range(h):
         for column in range(w):
-            part = conv_imgs[:, row:row + kh, column:column + kw] * kernel
+            part = images[:, row:row + kh, column:column + kw] * kernel
             conv[:, row, column] = np.sum(part, axis=(1, 2))
     return conv
