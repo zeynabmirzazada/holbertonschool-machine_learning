89c480290f48a8eba1206d001cc0e5476b01635f
root <root@b24267c86ff043faa5774228b18de820-2377118072>
2026-03-27 21:12:24 +0000

    dfg

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -17,7 +17,9 @@ def convolve_grayscale_padding(images, kernel, padding):
     Returns: a numpy.ndarray containing the convolved images'''
     m, h, w = images.shape[0], images.shape[1], images.shape[2]
     kh, kw = kernel.shape[0], kernel.shape[1]
-    images = np.pad(images, ((0, 0), (padding[0], padding[0]), (padding[1], padding[1])),
+    images = np.pad(images,
+                    ((0, 0), (padding[0], padding[0]),
+                        (padding[1], padding[1])),
                     constant_values=0)
     conv = np.zeros((m, images.shape[1] - kh + 1, images.shape[2] - kw + 1))
     for row in range(images.shape[1] - kh + 1):
