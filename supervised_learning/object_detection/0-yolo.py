2386a0b63de67362fdc38049aeadc30d59808d75
root <root@60d1a2257eac40c7b3de35d64c491e81-2377118072>
2026-04-29 21:17:31 +0000

    cnn

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -5,7 +5,7 @@ class Yolo:
   '''class'''
   def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
     '''instance attr'''
-    self.model = model_path
+    self.model = tensorflow.keras.models.load_model(model_path)
     self.class_names = classes_path
     self.class_t = class_t
     self.nms_t = nms_t
