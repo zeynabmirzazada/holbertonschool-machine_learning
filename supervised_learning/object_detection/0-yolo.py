c3e59549ab9384a61459699bdfdbada3103484a2
root <root@60d1a2257eac40c7b3de35d64c491e81-2377118072>
2026-04-29 22:02:59 +0000

    cnn

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -10,8 +10,8 @@ class Yolo:
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         '''instance attr'''
         self.model = tensorflow.keras.models.load_model(model_path)
-        self.class_names = [line.rstrip('\n') for line in 
-                open(classes_path, "r")]
+        self.class_names = [line.rstrip('\n') for line in/ 
+                            open(classes_path, "r")]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
