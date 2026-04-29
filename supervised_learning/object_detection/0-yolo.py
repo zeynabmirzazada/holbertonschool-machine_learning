44937e74630746cbd0f511c2c0924b936403d706
root <root@60d1a2257eac40c7b3de35d64c491e81-2377118072>
2026-04-29 21:04:33 +0000

    cnn

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -1,65 +1,12 @@
-## Resources
+#!/usr/bin/env python3
+'''initialize'''
 
-**Read or watch**:
-
-- [OpenCV](/rltoken/hpgxmSwXi5F8cznpAUPWwQ)
-- [Getting Started with Images](/rltoken/nXWMMsbEUZVF6OklI94pfg)
-- [Object Localization](/rltoken/vzbJ-rO55eL3Bm_6zKOCDA) 
-- [Landmark Detection](/rltoken/ubZcefcBGuLTxq8A-8eTVA) 
-- [Object Detection](/rltoken/4ePZRCJtg0JZ468--pl5FA) 
-- [Convolutional Implementation Sliding Windows](/rltoken/eUfNuLWkki2-WEzWP4Cp6A) 
-- [Intersection Over Union](/rltoken/kNRs-3gDzzvt7_M3mxoO_Q) 
-- [Nonmax Suppression](/rltoken/I8dOYuBvm22RfPnYHwz4gA) 
-- [Non-Maximum Suppression for Object Detection in Python](/rltoken/6PMZDVa2lcQdyQ03Tefq_w)
-- [Anchor Boxes](/rltoken/lBDD5xh2G7YK-lu4E_UV1w) 
-- [YOLO Algorithm](/rltoken/xQWgMbmb0lmJ8J3RDQPKGw) 
-- [You Only Look Once: Unified, Real-Time Object Detection (CVPR 2016)](/rltoken/mJhpyxNBVzaBcz9lqO8Kuw) 
-- [Real-time Object Detection with YOLO, YOLOv2 and now YOLOv3](/rltoken/Tsreto_BvGs3iOmTJuQKsQ) 
-- [Dive Really Deep into YOLO v3: A Beginner’s Guide](/rltoken/WkUR9PG8G7v7K7NbmuA7FQ)
-- [What’s new in YOLO v3?](/rltoken/EhWF1nNt9sy4GRMVWtXn-Q) 
-- [What do we learn from single shot object detectors (SSD, YOLOv3), FPN &amp; Focal loss (RetinaNet)?](/rltoken/3i2Pfnl_k2mmeTe7rVBI9g)
-- [mAP (mean Average Precision) for Object Detection](/rltoken/OY2vUmYJWqT7Ht5S4PeX_g) 
-- [You Only Look Once (YOLO): Unified Real-Time Object Detection](/rltoken/D50Dyw_bTcweH0zXYKkceQ)
-
-**Definitions to skim:**
-
-- [Object detection](/rltoken/8_jYvHSDi0aKxo2ea9zqAQ) 
-
-**References**:
-
-- [cv2.imshow](/rltoken/iwD5G_USXNFytFR7vssjGg)
-- [cv2.imread](/rltoken/mBDcRL2IAkzuYgTm9mdmEQ)
-- [cv2.line](/rltoken/8nBTlSNSnOLJl6G_UUYjqw)
-- [cv2.putText](/rltoken/vahl0T0CcMLfqERm9PdUHQ)
-- [cv2.rectangle](/rltoken/gkcY1CAKXPxLhTBXFpRcKg)
-- [cv2.resize](/rltoken/DsQ4ls96Bfs8oo9FKi5aBA)
-- [glob](/rltoken/RQEuZNp6TYgdQoeTHd5iAA)
-- [You Only Look Once: Unified, Real-Time Object Detection](/rltoken/AebIQZ3oGfGZozevXWzRng) 
-- [YOLO9000: Better, Faster, Stronger](/rltoken/YdFP59GdTIryU3sju-z5lA) 
-- [YOLOv3: An Incremental Improvement](/rltoken/XeUjWzVezQTmx5EcAg7lIQ) 
-
-**Advanced**:
- 
-- [Image segmentation](/rltoken/lzG4vdfjoohi8Mk4U4go6w)
-- [Region Proposals](/rltoken/zDEUJXfKFEsvvU2lftQtQg) 
-- [Understanding Feature Pyramid Networks for object detection (FPN)](/rltoken/60LbGymorGXltdD_JCaLsw)
-- [What do we learn from region based object detectors (Faster R-CNN, R-FCN, FPN)?](/rltoken/cMZ5VmdLvIGQqxqDXKnwKQ)
-- [Object Detection for Dummies Part 3: R-CNN Family](/rltoken/zOcXEIGcYVIRUUbvexcBOw) 
-- [Image segmentation with Mask R-CNN](/rltoken/tgdMUFwpJJclKCaicSMcwg)
-
-
-## Learning Objectives
-
-At the end of this project, you are expected to be able to [explain to anyone](/rltoken/lyV5uXZ6QBFjj48jtljPUw), __without the help of Google__:
-
-### General
-
-- What is OpenCV and how do you use it?
-- What is object detection?
-- What is the Sliding Windows algorithm?
-- What is a single-shot detector?
-- What is the YOLO algorithm?
-- What is IOU and how do you calculate it?
-- What is non-max suppression?
-- What are anchor boxes?
-- What is mAP and how do you calculate it?
+class Yolo:
+  '''class'''
+  def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+    '''instance attr'''
+    self.model = model_path
+    self.class_names = classes_path
+    self.class_t = class_t
+    self.nms_t = nms_t
+    self.anchors= anchors
