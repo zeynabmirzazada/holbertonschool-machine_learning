9fdcd61fcc633d2b5ea85a750b75485ccb85194e
root <root@a5eced72bfea45ab9db71b3d72347708-2377118072>
2026-05-24 08:39:10 +0200

    dfg

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """Calculates a GMM from a dataset"""
+    gmm = sklearn.mixture.GaussianMixture(n_components=k)
+    gmm.fit(X)
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+    return pi, m, S, clss, bic
