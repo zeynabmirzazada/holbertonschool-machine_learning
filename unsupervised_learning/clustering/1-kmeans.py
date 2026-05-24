03b5b098e5f460c7969f98618aea261504bf849b
root <root@a5eced72bfea45ab9db71b3d72347708-2377118072>
2026-05-24 08:19:18 +0200

    dfg

---
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/env python3
+"""Performs K-means"""
+import numpy as np
+
+
+def kmeans(X, k, iterations=1000):
+    """Performs K-means on a dataset
+        n is the number of data points
+        d is the number of dimensions for each data point """
+    if type(k) is not int or k <= 0:
+        return None, None
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(iterations) is not int or iterations <= 0:
+        return None, None
+    n, d = X.shape
+    centroids = np.random.uniform(np.min(X, axis=0), np.max(X, axis=0),
+                                  size=(k, d))
+    for i in range(iterations):
+        copy = centroids.copy()
+        D = np.sqrt(((X - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        clss = np.argmin(D, axis=0)
+        for j in range(k):
+            if len(X[clss == j]) == 0:
+                centroids[j] = np.random.uniform(np.min(X, axis=0),
+                                                 np.max(X, axis=0),
+                                                 size=(1, d))
+            else:
+                centroids[j] = (X[clss == j]).mean(axis=0)
+        D = np.sqrt(((X - centroids[:, np.newaxis]) ** 2).sum(axis=2))
+        clss = np.argmin(D, axis=0)
+        if np.all(copy == centroids):
+            return centroids, clss
+
+    return centroids, clss
