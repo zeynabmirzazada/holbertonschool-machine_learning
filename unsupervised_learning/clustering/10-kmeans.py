36920112aa24e158be5d044aff704af95d8a5c16
root <root@a5eced72bfea45ab9db71b3d72347708-2377118072>
2026-05-24 08:38:19 +0200

    dfg

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Comment of Function"""
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """Performs K-means on a dataset"""
+    kmeans = sklearn.cluster.KMeans(n_clusters=k)
+    kmeans.fit(X)
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+    return C, clss
