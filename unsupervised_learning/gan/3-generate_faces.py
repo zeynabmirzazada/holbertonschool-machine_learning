2b6988a76e9f1817ba4a2783f511f1db47d5ebe5
root <root@23d8eb29725647acb1f78cd4c0638a9f-2377118072>
2026-06-14 10:38:51 +0200

    xfbg

--- unsupervised_learning/gan/3-generate_faces.py
+++ unsupervised_learning/gan/3-generate_faces.py
@@ -1,168 +1,103 @@
 #!/usr/bin/env python3
-"""Module that defines a Wasserstein GAN with gradient penalty."""
-import tensorflow as tf
+"""
+    Module to create GAN model
+"""
 from tensorflow import keras
-import numpy as np
 
 
-class WGAN_GP(keras.Model):
-    """A Wasserstein GAN with gradient penalty."""
+def convolutional_GenDiscr():
+    """
+        function to create generator and discriminator for GAN model
+    :return:
+    """
 
-    def __init__(self, generator, discriminator, latent_generator,
-                 real_examples, batch_size=200, disc_iter=2,
-                 learning_rate=.005, lambda_gp=10):
-        """Initializes the WGAN_GP model.
-
-        Args:
-            generator: the generator network.
-            discriminator: the discriminator network.
-            latent_generator: function generating latent vectors.
-            real_examples: tensor of real examples.
-            batch_size: the batch size.
-            disc_iter: number of discriminator iterations per step.
-            learning_rate: the learning rate.
-            lambda_gp: the gradient penalty weight.
+    def conv_block_g(x,
+                     filters,
+                     kernel_size,
+                     strides=(1, 1),
+                     up_size=(2, 2),
+                     padding='same'
+                     ):
         """
-        super().__init__()
-        self.latent_generator = latent_generator
-        self.real_examples = real_examples
-        self.generator = generator
-        self.discriminator = discriminator
-        self.batch_size = batch_size
-        self.disc_iter = disc_iter
-
-        self.learning_rate = learning_rate
-        self.beta_1 = .3
-        self.beta_2 = .9
-
-        self.lambda_gp = lambda_gp
-        self.dims = self.real_examples.shape
-        self.len_dims = tf.size(self.dims)
-        self.axis = tf.range(1, self.len_dims, delta=1, dtype='int32')
-        self.scal_shape = self.dims.as_list()
-        self.scal_shape[0] = self.batch_size
-        for i in range(1, self.len_dims):
-            self.scal_shape[i] = 1
-        self.scal_shape = tf.convert_to_tensor(self.scal_shape)
-
-        self.generator.loss = lambda x: -tf.math.reduce_mean(x)
-        self.generator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate,
-            beta_1=self.beta_1, beta_2=self.beta_2
-        )
-        self.generator.compile(
-            optimizer=generator.optimizer, loss=generator.loss
-        )
-
-        self.discriminator.loss = lambda x, y: (
-            tf.math.reduce_mean(y) - tf.math.reduce_mean(x)
-        )
-        self.discriminator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate,
-            beta_1=self.beta_1, beta_2=self.beta_2
-        )
-        self.discriminator.compile(
-            optimizer=discriminator.optimizer, loss=discriminator.loss
-        )
-
-    def get_fake_sample(self, size=None, training=False):
-        """Generates a fake sample using the generator.
-
-        Args:
-            size: number of samples to generate.
-            training: whether the generator is in training mode.
-
-        Returns:
-            A batch of generated fake samples.
+            function to create conv block in generator
+        :param x: input
+        :param filters: int, number of filter
+        :param kernel_size: int, kernel size
+        :param strides: tuple, dor stride
+        :param up_size: tuple, size for upSampling2D layer
+        :param padding: string, name of padding's type
+        :return:
         """
-        if not size:
-            size = self.batch_size
-        return self.generator(self.latent_generator(size), training=training)
+        x = keras.layers.UpSampling2D(up_size)(x)
+        x = keras.layers.Conv2D(filters,
+                                kernel_size,
+                                strides,
+                                padding,
+                                activation='tanh')(x)
+        x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.Activation('tanh')(x)
+        return x
 
-    def get_real_sample(self, size=None):
-        """Gets a random real sample.
-
-        Args:
-            size: number of real samples to return.
-
-        Returns:
-            A batch of real samples.
+    def get_generator():
         """
-        if not size:
-            size = self.batch_size
-        sorted_indices = tf.range(tf.shape(self.real_examples)[0])
-        random_indices = tf.random.shuffle(sorted_indices)[:size]
-        return tf.gather(self.real_examples, random_indices)
-
-    def get_interpolated_sample(self, real_sample, fake_sample):
-        """Generates an interpolated sample between real and fake.
-
-        Args:
-            real_sample: a batch of real samples.
-            fake_sample: a batch of fake samples.
-
-        Returns:
-            A batch of interpolated samples.
+            function to construct the generator
+        :return: generator model
         """
-        u = tf.random.uniform(self.scal_shape)
-        v = tf.ones(self.scal_shape) - u
-        return u * real_sample + v * fake_sample
+        inputs = keras.Input(shape=(16,))
+        hidden = keras.layers.Dense(2048, activation='tanh')(inputs)
+        x = keras.layers.Reshape((2, 2, 512))(hidden)
+        x = conv_block_g(x, 64, (3, 3), (1, 1))
+        x = conv_block_g(x, 16, (3, 3))
+        outputs = conv_block_g(x, 1, (3, 3))
+        generator = keras.Model(inputs, outputs, name="generator")
+        return generator
 
-    def gradient_penalty(self, interpolated_sample):
-        """Computes the gradient penalty.
-
-        Args:
-            interpolated_sample: a batch of interpolated samples.
-
-        Returns:
-            The gradient penalty.
+    def conv_block_d(x,
+                     filters,
+                     kernel_size,
+                     strides=(2, 2),
+                     padding='same',
+                     pool_size=(2, 2)):
         """
-        with tf.GradientTape() as gp_tape:
-            gp_tape.watch(interpolated_sample)
-            pred = self.discriminator(interpolated_sample, training=True)
-        grads = gp_tape.gradient(pred, [interpolated_sample])[0]
-        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=self.axis))
-        return tf.reduce_mean((norm - 1.0) ** 2)
-
-    def train_step(self, useless_argument):
-        """Performs one training step of the WGAN_GP.
-
-        Args:
-            useless_argument: required by the Keras API but unused.
-
-        Returns:
-            A dictionary with the discriminator loss, generator loss,
-            and gradient penalty.
+            conv block for discriminator
+        :param x: input
+        :param filters: int, number of filter
+        :param kernel_size: int, size of kernel
+        :param strides: typle, value for stride
+        :param padding: string, name of padding's type
+        :param pool_size: tuple, size for Maxpooling2D layer
+        :return:
         """
-        for _ in range(self.disc_iter):
-            with tf.GradientTape() as tape:
-                real_sample = self.get_real_sample()
-                fake_sample = self.get_fake_sample(training=True)
-                interpolated_sample = self.get_interpolated_sample(
-                    real_sample, fake_sample
-                )
+        x = keras.layers.Conv2D(filters,
+                                kernel_size,
+                                (1, 1),
+                                padding)(x)
+        x = keras.layers.MaxPooling2D(pool_size, strides, padding)(x)
+        x = keras.layers.Activation('tanh')(x)
+        return x
 
-                real_output = self.discriminator(real_sample, training=True)
-                fake_output = self.discriminator(fake_sample, training=True)
-                discr_loss = self.discriminator.loss(real_output, fake_output)
-                gp = self.gradient_penalty(interpolated_sample)
-                new_discr_loss = discr_loss + self.lambda_gp * gp
-            grads = tape.gradient(
-                new_discr_loss, self.discriminator.trainable_variables
-            )
-            self.discriminator.optimizer.apply_gradients(
-                zip(grads, self.discriminator.trainable_variables)
-            )
+    def get_discriminator():
+        """
+            construct discriminator model
+        :return: discriminator model
+        """
+        inputs = keras.Input(shape=(16, 16, 1))
+        x = conv_block_d(inputs,
+                         32,
+                         (3, 3))
+        x = conv_block_d(x,
+                         64,
+                         (3, 3))
+        x = conv_block_d(x,
+                         128,
+                         (3, 3))
+        x = conv_block_d(x,
+                         256,
+                         (3, 3))
 
-        with tf.GradientTape() as tape:
-            fake_sample = self.get_fake_sample(training=True)
-            fake_output = self.discriminator(fake_sample, training=False)
-            gen_loss = self.generator.loss(fake_output)
-        grads = tape.gradient(
-            gen_loss, self.generator.trainable_variables
-        )
-        self.generator.optimizer.apply_gradients(
-            zip(grads, self.generator.trainable_variables)
-        )
+        x = keras.layers.Flatten()(x)
+        outputs = keras.layers.Dense(1, activation='tanh')(x)
+        discriminator = keras.Model(inputs, outputs, name="discriminator")
+        return discriminator
 
-        return {"discr_loss": discr_loss, "gen_loss": gen_loss, "gp": gp}
+    return get_generator(), get_discriminator()
