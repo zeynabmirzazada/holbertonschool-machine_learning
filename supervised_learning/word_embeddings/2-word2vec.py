f148cee0965489e25305342a7e1fa133f57fff76
root <root@4af6b49005ed49069f714882f7dfc2c2-2377118072>
2026-07-03 13:21:34 +0200

    xb

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -8,6 +8,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     '''model'''
     model = gensim.models.word2vec.Word2Vec(seed=seed, sentences=sentences,
             vector_size=vector_size, min_count=min_count, window=window,
-            negative=negative, sg=int(not(cbow)), epochs=epochs,
+            negative=negative, cbow=cbow, epochs=epochs,
             workers=workers)
     return model
