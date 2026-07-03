7f72a70b669306748b62313e245f111ffa675268
root <root@a229e9128f284549bc762b2cbbbb96bc-2377118072>
2026-07-03 22:36:36 +0200

    xcss

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -4,10 +4,15 @@ import gensim.models
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
-    window=5, cbow=True, epochs=5, seed=0, workers=1):
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
     '''model'''
     model = gensim.models.fasttext.FastText(sentences=sentences,
-            vector_size=vector_size, min_count=min_count, window=window,
-            negative=negative, sg=int(not(cbow)), epochs=epochs, seed=seed,
-            workers=workers)
+                                            vector_size=vector_size,
+                                            min_count=min_count,
+                                            window=window,
+                                            negative=negative,
+                                            sg=int(not(cbow)), epochs=epochs,
+                                            seed=seed, workers=workers)
+    model.build_vocab(corpus_iterable=sentences)
+    model.train(corpus_iterable=sentences, total_examples=len(sentences), epochs=epochs)
     return model
