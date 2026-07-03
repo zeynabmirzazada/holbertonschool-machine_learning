db95dfc6e1e40046b4c6ab013ebcae5cbeabd3b4
root <root@4af6b49005ed49069f714882f7dfc2c2-2377118072>
2026-07-03 11:38:28 +0200

    xb

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -7,12 +7,11 @@ def gensim_to_keras(model):
     '''hn'''
     keyed_vectors = model.wv  # structure holding the result of training
     weights = keyed_vectors.vectors  # vectors themselves, a 2D numpy array
-    index_to_key = keyed_vectors.index_to_key  # which row in `weights` corresponds to which word?
+    index_to_key = keyed_vectors.index_to_key  # which row in `weights` corre
 
     layer = tf.keras.layers.Embedding(
         input_dim=weights.shape[0],
         output_dim=weights.shape[1],
         weights=[weights],
-        trainable=False,
     )
     return layer
