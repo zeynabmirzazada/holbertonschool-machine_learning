4edc098e407949cef75f37802756afdae3faa17b
root <root@4af6b49005ed49069f714882f7dfc2c2-2377118072>
2026-07-03 13:06:16 +0200

    xb

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''word2vec'''
+import gensim.models
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+    window=5, cbow=True, epochs=5, seed=0, workers=1):
+    '''model'''
+    model = gensim.models.fasttext.FastText(sentences=sentences,
+            vector_size=vector_size, min_count=min_count, window=window,
+            negative=negative, sg=int(not(cbow)), epochs=epochs, seed=seed,
+            workers=workers)
+    return model
