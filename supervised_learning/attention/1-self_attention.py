f78d29af86eea0ae13163378dedcfe76189a55a1
root <root@2b5b089fb8564f67aca691bc573a0041-2377118072>
2026-07-09 00:49:30 +0200

    df

---
+++ supervised_learning/attention/1-self_attention.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Self Attention'''
+
+
+import tensorflow as tf
+
+
+class SelfAttention:
+    '''class of self attention'''
+    def __init__(self, units):
+        '''initialize'''
+        self.W = tf.keras.layers.Dense(units)
+        self.U = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1, activation='tanh')
+
+    def __call__(self, s_prev, hidden_states):
+        '''dbdfbdfxv'''
+        w = self.W(s_prev)
+        u = self.U(hidden_states)
+        v = self.V(u)
+        return w, v
