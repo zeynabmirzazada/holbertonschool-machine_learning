19df9f7ce62686ecdd4d8820e508ee965a5f547f
root <root@2b5b089fb8564f67aca691bc573a0041-2377118072>
2026-07-09 01:30:28 +0200

    df

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -17,5 +17,5 @@ class SelfAttention:
         '''dbdfbdfxv'''
         w = self.W(s_prev)
         u = self.U(hidden_states)
-        v = self.V(u)
+        v = self.V(tf.expand_dims(w, axis=1) + u)
         return w, v
