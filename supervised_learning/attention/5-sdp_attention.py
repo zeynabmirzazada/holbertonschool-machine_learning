e6406491541a6356b8af26d349c9bc4f8b14bcf9
root <157010106+zeynabmirzazada@users.noreply.github.com>
2026-07-10 00:29:41 +0200

    zdfgg

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 def sdp_attention(Q, K, V, mask=None):
     '''sc smncmjjmkjdm'''
-    a = tf.matmul(Q, K, transpose_b=True) /
+    a = (Q @ tf.transpose(K, perm=[0, 2, 1])) /
         tf.math.sqrt(tf.constant(float(K.shape[-1])))
     if mask != None:
         a += tf.math.multiply(mask, -1e9)
