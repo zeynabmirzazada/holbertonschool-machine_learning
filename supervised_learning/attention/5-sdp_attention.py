7911adbbec190b33172a870578976bfff1acfc05
root <root@214d03eea66e490b8ea4a27f9210ed31-2377118072>
2026-07-09 14:03:39 +0200

    fbfg

---
+++ supervised_learning/attention/5-sdp_attention.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''sdccvgkjsdcck'''
+
+
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    '''sc smncmjjmkjdm'''
+    a = tf.matmul(Q, K, transpose_b=True) / tf.math.sqrt(tf.constant(float(K.shape[-1])))
+    if mask != None:
+        a += mask * -1e9
+
+    weights = tf.nn.softmax(a, axis=-1)
+
+    output = tf.matmul(weights, V)
+    return output, weights
