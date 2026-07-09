5f83338ff27e6e4ae024a7f59327bcafd9b16d0c
root <root@214d03eea66e490b8ea4a27f9210ed31-2377118072>
2026-07-09 12:40:51 +0200

    fbfg

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -6,9 +6,9 @@ import numpy as np
 
 def positional_encoding(max_seq_len, dm):
     '''kvhjyfhfukmkm'''
-    angle_rads = np.arange(position)[:, np.newaxis] / np.power(
-        10000, (2 * (np.arange(d_model)[np.newaxis, :] // 2)) / 
-            np.float32(d_model)
+    angle_rads = np.arange(max_seq_len)[:, np.newaxis] / np.power(
+        10000, (2 * (np.arange(dm)[np.newaxis, :] // 2)) / 
+            np.float32(dm)
     )
     angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
     angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
