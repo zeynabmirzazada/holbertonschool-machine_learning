df11eee35b839cb979a91a49ac4556e4471a6455
root <157010106+zeynabmirzazada@users.noreply.github.com>
2026-07-12 18:28:31 +0200

    dfvfbz

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
-'''Self Attention'''
-
-
+"""Self Attention"""
 import tensorflow as tf
 
 
-class SelfAttention:
-    '''class of self attention'''
+class SelfAttention(tf.keras.layers.Layer):
+    """Self Attention for Encoder Decoder architechture"""
+
     def __init__(self, units):
-        '''initialize'''
+        """Initialize the class"""
+        super().__init__()
         self.W = tf.keras.layers.Dense(units)
         self.U = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
 
-    def __call__(self, s_prev, hidden_states):
-        '''dbdfbdfxv'''
-        w = self.W(s_prev)
-        u = self.U(hidden_states)
-        v = self.V(tf.math.tanh(tf.expand_dims(w, axis=1) + u))
-        return w, v
+    def call(self, s_prev, hidden_states):
+        """Call the layer"""
+        s_prev = tf.expand_dims(s_prev, 1)
+
+        W = self.W(s_prev)
+        U = self.U(hidden_states)
+
+        alignement_output = self.V(tf.math.tanh(W + U))
+        weights = tf.math.softmax(alignement_output, axis=1)
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
+
+        return context, weights
