395c3216425691d80335db43c97c8f15ff36e544
root <157010106+zeynabmirzazada@users.noreply.github.com>
2026-07-18 23:26:42 +0200

    dfg

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''svdfdbnhmksk'''
+import transformers
+from setup import load_pt2en
+
+
+class Dataset:
+    '''this is class'''
+    def __init__(self):
+        '''wdccjncdnkdkdj'''
+        self.data_train = load_pt2en('train')
+        self.data_valid = load_pt2en('validation')
+        self.tokenizer_pt = train_new_from_iterator(self.data_train)
+        self.tokenizer_en = train_new_from_iterator(self.data_train)
+
+    def tokenize_dataset(self, data):
+        '''sjfjjdfjdkjehuytsyf'''
+
