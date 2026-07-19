dcc17cfe7ebe0ec7e8504694a1b2e483ae3a21f1
root <157010106+zeynabmirzazada@users.noreply.github.com>
2026-07-19 11:22:29 +0200

    kkj

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -8,8 +8,8 @@ class Dataset:
     '''this is class'''
     def __init__(self):
         '''wdccjncdnkdkdj'''
-        self.data_train = load_pt2en('train')
-        self.data_valid = load_pt2en('validation')
+        self.data_train = load_pt2en(split='train')
+        self.data_valid = load_pt2en(split='validation')
         self.tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
                 'neuralmind/bert-base-portuguese-cased')
         self.tokenizer_en = transformers.AutoTokenizer.from_pretrained(
